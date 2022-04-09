9d3e76369f681543e137786f2f9ffd715bb44f87
overdrive80 <lukillas.ole@gmail.com>
2022-04-09 11:13:06 +0200

    Update vscomparevideos.py

--- vscomparevideos.py
+++ vscomparevideos.py
@@ -2,7 +2,7 @@ import vapoursynth as vs
 from vapoursynth import VideoNode
 core = vs.core
 
-def comparevideos(clip_1: VideoNode, clip_2: VideoNode, show: bool = True):
+def comparevideos(clip_1: VideoNode, clip_2: VideoNode, clip_name1 = None, clip_name2 = None, show: bool = True):
     
 	# If is False return clip 2
 	if show == False:
@@ -13,11 +13,16 @@ def comparevideos(clip_1: VideoNode, clip_2: VideoNode, show: bool = True):
 		raise ValueError("The parameter 'clip_1' is not a clip")
 	if not isinstance(clip_2, vs.VideoNode):
 		raise ValueError("The parameter 'clip_2' is not a clip")
-	
+        
+	if clip_name1 is None:
+		clip_name1 = "Clip 1"
+	if clip_name2 is None:
+		clip_name2 = "Clip 2"
+        
 	# If both parameters are clips
 	try:
-		clip_1 = core.sub.Subtitle(clip_1, "Clip 1")
-		clip_2 = core.sub.Subtitle(clip_2, "Clip 2")
+		clip_1 = core.sub.Subtitle(clip_1, clip_name1)
+		clip_2 = core.sub.Subtitle(clip_2, clip_name2)
 		interleave = core.std.Interleave([clip_1,clip_2])
 		return interleave
 	except Exception as e:
